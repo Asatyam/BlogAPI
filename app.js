c04defa5611c689a89633a5d0b9ed12d1d8bb48e
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-23 17:14:20 +0530

    revert to session

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors());
 
 
 
-app.use(cookie({secret: process.env.SECRET, resave:false, saveUninitialized: true}));
+app.use(session({secret: process.env.SECRET, resave:false, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
